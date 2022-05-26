9c62da00bb5e4d25995d7581e5c1d54ee230a328
kdabake <kdabake@crexendo.com>
2022-05-26 18:08:16 +0530

    Iframe changes

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -9,7 +9,7 @@
     </div>
     
     <div class="col-sm-12 col-lg-6" style="border: solid #ffc107 3px; overflow: hidden">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d472.296719035993!2d73.09674428784496!3d18.736835297051794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be80b29d266136b%3A0x7bf3c9a9dc128588!2sKasar%20Ali%20Rd%2C%20Ziral%20Ali%2C%20Pen%2C%20Maharashtra%20402107!5e0!3m2!1sen!2sin!4v1653494180070!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d472.296719035993!2d73.09674428784496!3d18.736835297051794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be80b29d266136b%3A0x7bf3c9a9dc128588!2sKasar%20Ali%20Rd%2C%20Ziral%20Ali%2C%20Pen%2C%20Maharashtra%20402107!5e0!3m2!1sen!2sin!4v1653494180070!5m2!1sen!2sin" width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   </div>
   </div>
